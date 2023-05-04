a5275a2a58de7a67a470e969aef7901f9c7a44dd
RONJA06 <132578856+oleg228888@users.noreply.github.com>
2023-05-04 16:00:45 +0300

    Initial commit

---
+++ Assets/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    bool isGround = true;
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, 0);
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rb.AddForce(new Vector2(0, jumpForce),
+                ForceMode2D.Impulse);
+        }
+    }
+}
